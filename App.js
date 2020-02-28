5a253160ff31b8cfbfce471a818398369238361b
labuz.ivan <i.lubuz.exceedteam@gmail.com>
2020-02-28 18:59:51 +0300

    todo

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css';
-import { array } from 'prop-types';
 
 class App extends React.Component {
   constructor(props){
     super(props);
-  this.state = {
-    todoList : [],
-    inputText : '',
+    this.state = {
+      todoList : [],
+      inputText : '',
+      switch : 'all',
+      selectedIndex: null,
+      magicBox : true,
     } 
   }
 onInput = (event) => {
   this.setState({
     inputText : event.target.value
-  }
-  )
+  })
 }
+
 
 onEnter = (event) => {
   if (event.key === "Enter" && this.state.inputText !== ""){
@@ -46,56 +48,143 @@ onDelete = (index) => {
   })
 }
 
+addAllButton = (event) => { 
+  let newArray = [...this.state.todoList]
+  this.state.todoList.map(function (currentValue) { 
+    currentValue.active = event.target.checked
+})  
+  this.setState({
+  todoList : newArray,
+  magicBox : event.target.checked,
+  })   
+}
+
+
+// Double = (event) => {
+//   this.setState({
+//     inputText : inputTextNew,
+//   })
+  
+// }
+// onEnterNew = (event) => {
+//   if (event.key === "Enter" && this.state.inputText !== ""){
+//     this.setState({
+//       inputText : inputTextNew
+//     })
+// }
+// }
+
 addArray = () =>{
-  const mapArray = this.state.todoList.map(
-    (todo, index) => {
-      return(
-      <p>
-        <input 
-        type='checkbox'
-        class="strikethrough"
-        checked= {todo.active}
-        onChange={(event) => this.clickBox(event, index)}
-        />
-        <span>{todo.name}</span>
-        <button 
-        className={'deletebutton'} 
-        onClick= {() => this.onDelete(index)}
-        >Delete</button>
-      </p>
-    )
+  const all = this.state.todoList;
+  const checked = this.state.todoList.filter(word => word.active === true);
+  const notChecked = this.state.todoList.filter(word => word.active === false);
+
+  let filteredArray;
+  if(this.state.switch === 'all'){
+    filteredArray = all;
   }
-)
+  if(this.state.switch === 'notCompleted'){
+    filteredArray = notChecked;
+  }
+  if(this.state.switch === 'completed'){
+    filteredArray = checked;
+  }
+  const mapArray = filteredArray.map(
+    (todo, index) => {
+      console.log(this.onInputNew)
+      return(
+        <div className= 'clearVision'
+        >
+          <input 
+            type='checkbox'
+            className="strikethrough"
+            checked= {todo.active}
+            onChange={(event) => this.clickBox(event, index)}
+          />
+          <div
+          onDoubleClick= {() =>this.Double()}
+          // className= {this.state.switch === '' ? '' : ''}
+          >{todo.name}</div>
+          <input 
+          value = {this.inputTextNew}
+          onChange = {this.onInputNew}
+          onKeyPress = {this.onEnterNew} 
+          />
+          <button 
+            className={'deleteButton'} 
+            onClick= {() => this.onDelete(index)}
+          >X</button>
+        </div>
+      )
+    }
+  )
   return mapArray
 }
 
+clearCompleted = () => {
+  const notActive = this.state.todoList.filter(word => word.active === false);
+  this.setState({
+    todoList : notActive
+  })
+}
+
+onSelectSwitch = (val) => {
+  this.setState({
+    switch : val
+  })
+}
 onPool = () => {
-  console.log('hello')
-  const onCounter = this.state.todoList.length;
+  const result = this.state.todoList.filter(word => word.active === false);
+  const count = result.length
+  const clCmp = this.state.todoList.filter(word => word.active === true);
+  const clResult = clCmp.length
   return(
     <div className= {'divFooter'}>
-    <span>{onCounter} items left</span>
-    <button>All</button>
-    <button>Active</button>
-    <button>Completed</button>
-    <button className= {'clear'}>Clear Completed</button>
-
+    <span>{count} items left</span>
+    <button
+      className= {this.state.switch === 'all' ? 'active' : ''}
+      onClick= {() => this.onSelectSwitch('all')}
+    >All</button>
+    <button
+      className= {this.state.switch === 'notCompleted' ? 'active' : ''}
+      onClick= {() => this.onSelectSwitch('notCompleted')}
+    >Active</button>
+    <button
+      className= {this.state.switch === 'completed' ? 'active' : ''}
+      onClick= {() => this.onSelectSwitch('completed')}  
+    >Completed</button>
+    <button 
+      className= {clResult !== 0  ? 'clear' : 'clearOnVision'}
+      onClick = {(event) => this.clearCompleted(event)}
+    >Clear Completed</button>
     </div>
     )
-  }
+  } 
 
 
  render() { 
   return (
     <div className = {'div_todo'}>
-    <input
-    className = {'add_todo'}
-    value = {this.state.inputText}
-    onChange = {this.onInput}
-    onKeyPress= {this.onEnter} 
-    placeholder= 'What needs be done'/> 
-    <span>{this.addArray()}</span>
-    <div>{this.onPool()}</div>
+      <div>
+        <h1 
+        className= {'todos'}
+        >todos</h1>
+        <input
+        type= 'checkbox'
+        value= {this.state.magicBox}
+        className= {'add_allbutton'}
+        onChange= {(event) => this.addAllButton(event)}
+        ></input>
+        <input
+          className = {'add_todo'}
+          value = {this.state.inputText}
+          onChange = {this.onInput}
+          onKeyPress = {this.onEnter} 
+          placeholder = 'What needs be done'
+        /> 
+      </div>  
+      <span>{this.addArray()}</span>
+      <div>{this.onPool()}</div>
     </div>
   );
  }
